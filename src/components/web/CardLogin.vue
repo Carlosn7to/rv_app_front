807565bc600a3f0d24a83877469761543209ee0c
Carlos Neto <carlosn7to@gmail.com>
2022-07-24 20:28:01 -0300

    first commit

--- src/components/web/CardLogin.vue
+++ src/components/web/CardLogin.vue
@@ -4,8 +4,12 @@
       <img :src="require('@/assets/company/logo_azul_laranja.png')" alt="logo_age">
     </div>
     <div id="form">
-      <input type="text" name="email" id="email" placeholder="E-mail">
-      <input type="password" name="password" id="password" placeholder="Senha">
+      <input
+          v-model="email"
+          type="text" name="email" id="email" placeholder="E-mail">
+      <input
+          v-model="password"
+          type="password" name="password" id="password" placeholder="Senha">
       <input type="submit" value="Entrar" @click="authentic">
     </div>
 
@@ -18,22 +22,36 @@ import {AXIOS} from "../../../services/api.ts";
 
 export default {
   name: "CardLogin",
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
   methods: {
     authentic: function () {
+
+      const payload = {
+        email: this.email,
+        password: this.password
+      }
+
+      console.log(payload)
+
       AXIOS({
         method: "POST",
         url: "login",
         headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        }
+          'Content-Type': 'application/json',
+          'Access': 'application/json',
+        },
+        data: JSON.stringify(payload)
       })
           .then((res) => {
             console.log(res)
           })
-          .catch((error) => {
-            console.log(error)
-          })
+
+
     }
   }
 }
