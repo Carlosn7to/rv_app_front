57b273db2144e4dddf6a31dc54eada37f7486918
Carlos Neto <carlosn7to@gmail.com>
2022-08-02 18:13:06 -0300

    bastante mudança

--- src/views/app/sales/MySales.vue
+++ src/views/app/sales/MySales.vue
@@ -9,21 +9,21 @@
           <div id="dashboards">
             <div class="dashboard" style="background: #FEA11D;">
               <div>
-                <span>281</span>
+                <span>{{ dashboard.stars.value }}</span>
                 <h6>Estrelas</h6>
               </div>
               <i class="fi fi-ss-star"></i>
             </div>
             <div class="dashboard" style="background-color: #009688">
               <div>
-                <span>{{ dashboard.sales }}</span>
-                <h6>Vendas</h6>
+                <span>{{ dashboard.sales.count }}</span>
+                <h6>Vendas válidas</h6>
               </div>
               <i class="fi fi-ss-rocket-lunch"></i>
             </div>
             <div class="dashboard" style="background-color: #3F51B5">
               <div>
-                <span>{{ dashboard.plan.name }}({{dashboard.plan.un}})</span>
+                <span>{{ dashboard.plan.name }}</span>
                 <h6>Top plano</h6>
               </div>
               <i class="fi fi-ss-shopping-cart-check"></i>
@@ -36,40 +36,100 @@
               <i class="fi fi-ss-delete-document"></i>
             </div>
           </div>
+          <div id="dashboards">
+            <div class="dashboard" style="background: rgba(0,150,2,0.85);">
+              <div>
+                <span>R${{ dashboard.stars.amount }}</span>
+                <h6>Comissão</h6>
+              </div>
+              <i class="fi fi-sr-sack-dollar"></i>
+            </div>
+            <div class="dashboard" style="background-color: #feca1d">
+              <div>
+                <span>R${{ dashboard.stars.price }}</span>
+                <h6>Valor da estrela</h6>
+              </div>
+              <i class="fi fi-sr-grin-stars"></i>
+            </div>
+            <div class="dashboard" style="background-color: #983fb5">
+              <div>
+                <span>{{ dashboard.stars.meta }}%</span>
+                <h6>Meta atingida</h6>
+              </div>
+              <i class="fi fi-sr-chart-line-up"></i>
+            </div>
+            <div class="dashboard" style="background-color: rgba(145,145,145,0.65)">
+              <div>
+                <span>0%</span>
+                <h6>Deflator</h6>
+              </div>
+              <i class="fi fi-sr-settings-sliders"></i>
+            </div>
+          </div>
+          <div class="content-sales">
+            <div id="list-sales">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Nº contrato</th>
+                    <th style="text-align: left">Nome do cliente</th>
+                    <th style="text-align: left">Plano</th>
+                    <th>Estrelas</th>
+                    <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="sale in dashboard.sales.sales" :key="sale.id_contrato">
+                    <td>{{ sale.id_contrato }}1</td>
+                    <td style="text-align: left">{{ sale.nome_cliente }}</td>
+                    <td style="text-align: left">{{ sale.plano }}</td>
+                    <td>{{ sale.estrela }}</td>
+                    <td>{{ sale.status }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div id="calc-sales">
+              <h6>Projeção de vendas</h6>
+              <p>Hoje é dia <b>02</b>, faltam <b>28</b> dias para acabar seu mês e
+                caso você mantenha esse desempenho, terminará o mês com <b>0</b> estrelas, que dará uma comissão de <b>R$0,00</b>
+              </p>
+            </div>
+          </div>
 
         </div>
         <div id="filter">
           <div class="itens-filter">
             <h6>Ano</h6>
             <div id="years">
-              <span class="itemFilter" :class="{ isActive : filter.year === 2021 }" @click="filterYear(2021)">2021</span>
+<!--              <span class="itemFilter" :class="{ isActive : filter.year === 2021 }" @click="filterYear(2021)">2021</span>-->
               <span class="itemFilter" :class="{ isActive : filter.year === 2022 }" @click="filterYear(2022)">2022</span>
             </div>
           </div>
           <div class="itens-filter">
             <h6>Mês</h6>
             <div class="months">
-              <span class="itemFilter" :class="{ isActive : filter.month === '01' }" @click="filterMonth('01')">Jan</span>
-              <span class="itemFilter" :class="{ isActive : filter.month === '02' }" @click="filterMonth('02')">Fev</span>
-              <span class="itemFilter" :class="{ isActive : filter.month === '03' }" @click="filterMonth('03')">Mar</span>
-              <span class="itemFilter" :class="{ isActive : filter.month === '04' }" @click="filterMonth('04')">Abr</span>
+<!--              <span class="itemFilter" :class="{ isActive : filter.month === '01' }" @click="filterMonth('01')">Jan</span>-->
+<!--              <span class="itemFilter" :class="{ isActive : filter.month === '02' }" @click="filterMonth('02')">Fev</span>-->
+<!--              <span class="itemFilter" :class="{ isActive : filter.month === '03' }" @click="filterMonth('03')">Mar</span>-->
+<!--              <span class="itemFilter" :class="{ isActive : filter.month === '04' }" @click="filterMonth('04')">Abr</span>-->
               <span class="itemFilter" :class="{ isActive : filter.month === '05' }" @click="filterMonth('05')">Mai</span>
               <span class="itemFilter" :class="{ isActive : filter.month === '06' }" @click="filterMonth('06')">Jun</span>
               <span class="itemFilter" :class="{ isActive : filter.month === '07' }" @click="filterMonth('07')">Jul</span>
               <span class="itemFilter" :class="{ isActive : filter.month === '08' }" @click="filterMonth('08')">Ago</span>
-              <span class="itemFilter" :class="{ isActive : filter.month === '09' }" @click="filterMonth('09')">Set</span>
-              <span class="itemFilter" :class="{ isActive : filter.month === '10' }" @click="filterMonth('10')">Out</span>
-              <span class="itemFilter" :class="{ isActive : filter.month === '11' }" @click="filterMonth('11')">Nov</span>
-              <span class="itemFilter" :class="{ isActive : filter.month === '12' }" @click="filterMonth('12')">Dez</span>
+<!--              <span class="itemFilter" :class="{ isActive : filter.month === '09' }" @click="filterMonth('09')">Set</span>-->
+<!--              <span class="itemFilter" :class="{ isActive : filter.month === '10' }" @click="filterMonth('10')">Out</span>-->
+<!--              <span class="itemFilter" :class="{ isActive : filter.month === '11' }" @click="filterMonth('11')">Nov</span>-->
+<!--              <span class="itemFilter" :class="{ isActive : filter.month === '12' }" @click="filterMonth('12')">Dez</span>-->
             </div>
           </div>
-          <div class="itens-filter">
-            <h6>Status</h6>
-            <div class="months">
-              <span class="itemFilter" :class="{ isActive : filter.status === 'Aprovado' }" @click="filterStatus('Aprovado')">Aprovado</span>
-              <span class="itemFilter" :class="{ isActive : filter.status === 'Cancelado' }" @click="filterStatus('Cancelado')">Cancelado</span>
-            </div>
-          </div>
+<!--          <div class="itens-filter">-->
+<!--            <h6>Status</h6>-->
+<!--            <div class="months">-->
+<!--              <span class="itemFilter" :class="{ isActive : filter.status === 'Aprovado' }" @click="filterStatus('Aprovado')">Aprovado</span>-->
+<!--              <span class="itemFilter" :class="{ isActive : filter.status === 'Cancelado' }" @click="filterStatus('Cancelado')">Cancelado</span>-->
+<!--            </div>-->
+<!--          </div>-->
           <div class="itens-filter" style="border: none">
             <button @click="getFilterData">Filtrar</button>
           </div>
@@ -100,8 +160,16 @@ export default {
         status: null
       },
       dashboard: {
-        stars: null,
-        sales: null,
+        stars: {
+          value: null,
+          amount: null,
+          price: null,
+          meta: null
+        },
+        sales: {
+          count: null,
+          sales: null
+        },
         plan: {
           name: null,
           un: null
@@ -140,8 +208,12 @@ export default {
             status: this.filter.status
           }
         }).then((res) => {
-          console.log(res.data)
-          this.dashboard.sales = res.data.dashboard.sales
+          this.dashboard.sales.count = res.data.dashboard.sales
+          this.dashboard.sales.sales = res.data.sales
+          this.dashboard.stars.value = res.data.dashboard.stars.original.stars
+          this.dashboard.stars.amount = res.data.dashboard.stars.original.comission
+          this.dashboard.stars.price = res.data.dashboard.stars.original.price
+          this.dashboard.stars.meta = res.data.dashboard.stars.original.meta
           this.dashboard.plan.name = res.data.dashboard.plan
           this.dashboard.plan.un = res.data.dashboard.plan_qntd
           this.dashboard.cancel = res.data.dashboard.cancelled
@@ -170,6 +242,7 @@ export default {
   #sales {
     width: 85%;
     height: 100%;
+    @include flex(column, initial, initial, 20px);
 
     #dashboards {
       height: 12%;
@@ -186,19 +259,69 @@ export default {
 
         h6 {
           font-size: 1.4rem;
-          color: rgba(255, 255, 255, .8);
+          color: rgba(255, 255, 255, .93);
           font-weight: 500;
         }
 
         span {
           font-size: 2.4rem;
-          font-weight: 400;
+          font-weight: 700;
           color: #FFF;
         }
 
         i {
           font-size: 4.2rem;
-          color: #FFFFFF80
+          color: #FFFFFF80;
+        }
+      }
+    }
+
+    .content-sales {
+      @include flex(row, initial, initial, 20px);
+      width: 100%;
+      height: 70%;
+
+      #calc-sales {
+        width: 30%;
+        height: 40%;
+        padding: 15px;
+        border-radius: 10px;
+        background-color: #fff;
+
+          h6 {
+            font-size: 3.2rem;
+            color: $text;
+            margin-bottom: 2vh;
+          }
+
+          p {
+            font-size: 2rem;
+            color: $text-menu;
+
+            b {
+              font-size: 2.2rem;
+              color: #333;
+            }
+          }
+      }
+
+      #list-sales {
+        width: 70%;
+        background-color: #fff;
+        padding: 10px;
+        border-radius: 10px;
+
+        table {
+          @include table;
+          text-align: center;
+
+          thead {
+            tr {
+              th {
+                text-align: center;
+              }
+            }
+          }
         }
       }
     }
@@ -258,13 +381,14 @@ export default {
         margin: 2vh 0;
         background-color: $age-blue;
         color: #fff;
-        padding: 5px 50px;
+        padding: 5px 0px;
         border: 2px solid #fff;
         border-radius: 5px;
         font-size: 1.6rem;
         font-weight: 600;
         cursor: pointer;
         @include transtion;
+        width: 80%;
 
         &:hover {
           border: 2px solid $age-blue;
