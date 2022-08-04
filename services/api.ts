5bd171ff762a03c93fae6143d2a65a835397f371
Carlos Neto <carlosn7to@gmail.com>
2022-08-04 11:00:00 -0300

    production

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const AXIOS = axios.create({
-    baseURL: `http://localhost:8000/api/`,
+    baseURL: `http://v1.agerv.tote.local/api/`,
     headers: {
 
     }
