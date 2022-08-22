07d2be995bbafd2f0b51744180cf05af11caa88c
Carlos Neto <carlosn7to@gmail.com>
2022-08-22 15:39:21 -0300

    https agerv

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const AXIOS = axios.create({
-    baseURL: `https://v1.agerv.tote.local/api/`,
+    baseURL: `https://v1.agerv.agetelecom.com.br/api/`,
     //baseURL: `http://localhost:8000/api/`,
     headers: {
 
